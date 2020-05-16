2978ad30525e9ef07d130ce0942e405d49a1aecc
Ryan Pearson <ryanpinpb@gmail.com>
2020-05-16 13:10:50 -0700

    using jsonplaceholder to send dummy get/post requests to interact w/ blog and fake database

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +8,61 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Ryan',
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            clicked={() => this.postSelectedHandler(post.id)}
+            key={post.id}
+            title={post.title}
+            author={post.author}
+          />
         );
+      });
     }
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
